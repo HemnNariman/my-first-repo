e1fd3b7e5b6a1d0cc2884d7a7aed06aa25fc34bb
HemnNariman <learning1125r@gmail.com>
2025-02-20 15:27:43 +0300

    my-first-repo

---
+++ Calc.java
@@ -0,0 +1,57 @@
+
+
+import java.util.Scanner;
+
+public class Calc {
+
+  public static void main(String[] args) {
+
+    Scanner Calc = new Scanner(System.in);
+
+    System.out.println("Enter first number: ");
+
+    double num1 = Calc.nextDouble();
+
+    System.out.println("Enter one of these operators (+,-,/,*,%)");
+
+    char op = Calc.next().charAt(0);
+
+    System.out.println("Enter your second number: ");
+
+    double num2 = Calc.nextDouble();
+
+    switch (op) {
+      case '+':
+        System.out.println("the result is: " + (num1 + num2));
+        break;
+      case '-':
+        System.out.println("the result is: " + (num1 - num2));
+        break;
+      case '*':
+        System.out.println("the result is: " + (num1 * num2));
+        break;
+      case '/':
+        if (num2 == 0) {
+          System.out.println("Error: Division by zero is not allowed.");
+        } else {
+          System.out.println("the result is: " + (num1 / num2));
+        }
+        break;
+      case '%':
+        if (num2 == 0) {
+          System.out.println("Error: Modulus by zero is not allowed.");
+        } else {
+          System.out.println("the result is: " + (num1 % num2));
+        }
+        break;
+
+      default:
+        System.out.println("Enter only these operators (+,-,/,*,%)");
+
+    }
+
+    Calc.close();
+
+  }
+
+}
