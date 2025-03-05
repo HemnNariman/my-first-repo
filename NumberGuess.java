fa0825422154f791b0dd7c52318abced50854027
HemnNariman <learning1125r@gmail.com>
2025-03-05 13:34:54 +0300

    my-first-repo

---
+++ NumberGuess.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class NumberGuess {
+  public static void main(String[] args) {
+    // NUMBER GUESSING GAME
+
+    Scanner scanner = new Scanner(System.in);
+    Random random = new Random();
+    int guess;
+    int attempts = 0;
+    int min = 1;
+    int max = 100;
+    int randomNumber = random.nextInt(min, max + 1);
+
+    System.out.println("Number Guessing Game");
+    System.out.printf("Guess a number between %d-%d:\n", min, max);
+
+    do {
+      System.out.print("Enter a guess: ");
+      guess = scanner.nextInt();
+      attempts++;
+
+      if (guess < randomNumber) {
+        System.out.println("TOO LOW! try again");
+      } else if (guess > randomNumber) {
+        System.out.println("TOO HIGH! try again");
+      } else {
+        System.out.println("CORRECT! the number was " + randomNumber);
+        System.out.println("# of attempts: " + attempts);
+      }
+
+    } while (guess != randomNumber);
+
+    scanner.close();
+  }
+}
