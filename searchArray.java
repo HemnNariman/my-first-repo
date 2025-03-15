d89dbae67b9aca6a78080634e18b5d82e67d5d52
HemnNariman <learning1125r@gmail.com>
2025-03-15 06:16:00 +0300

    my-first-repo

---
+++ searchArray.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class searchArray {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    String[] fruits = { "apple", "orange", "banana", "watermelon", "grapes" };
+    boolean isFound = false;
+    String target;
+
+    System.out.print("Enter a fruit to search for: ");
+    target = scanner.nextLine();
+
+    for (int i = 0; i < fruits.length; i++) {
+      if (fruits[i].equals(target)) {
+        System.out.println("Element found at index: " + i);
+        isFound = true;
+        break;
+      }
+    }
+    if (!isFound) {
+
+      System.out.println("Element not found in the array");
+    }
+    scanner.close();
+  }
+}
