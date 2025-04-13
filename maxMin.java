8e1fe17b34b6769707b6dd9fba0c6b7a5b08e9ae
HemnNariman <learning1125r@gmail.com>
2025-04-13 16:38:26 +0300

    my-first-repo

---
+++ maxMin.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class maxMin {
+  public static void main(String[] args) {
+
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.print("Enter first number: ");
+    double num1 = scanner.nextDouble();
+    System.out.print("Enter second number: ");
+    double num2 = scanner.nextDouble();
+    System.out.print("Enter third number: ");
+    double num3 = scanner.nextDouble();
+
+    if (num1 > num2 && num1 > num3) {
+      System.out.println(num1 + " is the maximum number.");
+    } else if (num2 > num1 && num2 > num3) {
+      System.out.println(num2 + " is the maximum number.");
+    } else if (num3 > num1 && num3 > num2) {
+      System.out.println(num3 + " is the maximum number.");
+    } else {
+      System.out.println("All numbers are equal");
+    }
+
+    scanner.close();
+  }
+}
